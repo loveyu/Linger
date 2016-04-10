e90a75b010689b64b06d3d428c9e190dd29792bb
loveyu <admin@loveyu.info>
2016-04-11 01:03:01 +0800

    准备自动发布工具

---
+++ auto_tools/post_gallery/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: loveyu
+ * Date: 2016/4/11
+ * Time: 0:43
+ */
+return array(
+	'dir_path' => 'F://Pictures/壁纸/',
+	'url' => 'http://pitus.loc/'
+);
---
+++ auto_tools/post_gallery/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 自动图集发布
+ * User: loveyu
+ * Date: 2016/4/11
+ * Time: 0:20
+ */
+$config = array_merge([
+	'dir_path' => '',
+	'url' => '',
+	''
+], include __DIR__ . "/config.php");
