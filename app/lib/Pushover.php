21ec135efef1aa96e94a605a9403b8ba239237ef
loveyu <admin@loveyu.info>
2016-08-20 02:43:51 +0800

    调整参数错误的问题

--- app/lib/Pushover.php
+++ app/lib/Pushover.php
@@ -15,6 +15,11 @@ namespace ULib;
 class Pushover{
 
 	/**
+	 * @var array 默认配置
+	 */
+	private static $cfg = [];
+
+	/**
 	 * 推送一个公共消息到某一私有服务器
 	 * @param string $title   消息标题
 	 * @param string $content 消息内容
@@ -22,14 +27,15 @@ class Pushover{
 	 * @return int 返回一个标示
 	 */
 	public static function push($title, $content, $delay = 0){
-		$cfg = array_merge(array(), cfg()->get('pushover_beanstalk_cfg'));
+		$current_cfg = cfg()->get('pushover_beanstalk_cfg');
+		$cfg = array_merge(self::$cfg, is_array($current_cfg) ? $current_cfg : []);
 		$client = new Beanstalk($cfg);
 		$client->connect();
-		$id = $client->put(0, $delay, 0, json_encode(array(
+		$id = $client->put(0, $delay, 0, json_encode([
 			'type' => 'pushover',
 			'content' => $content,
 			'title' => $title,
-		)));
+		]));
 		$client->disconnect();
 		return (int)$id;
 	}
